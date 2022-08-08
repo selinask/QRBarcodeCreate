ddb57f987f8fd51f199377c72ccc7a0cf99f4a95
Selin AŞIK <sln_124@hotmail.com>
2022-08-08 22:26:44 +0300

    Add project files.

---
+++ QRBarcodeCreate/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QRBarcodeCreate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            pictureBox1.Image = qrCodeImage;
+        }
+    }
+
+}
